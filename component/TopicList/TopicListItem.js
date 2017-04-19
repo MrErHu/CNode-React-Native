2de6706f625c4b917c293c23a8e15d84abed9edb
MrErHu <wanglei_cs@163.com>
2017-04-19 23:50:53 +0800

    update TopicListItem

--- component/TopicList/TopicListItem.js
+++ component/TopicList/TopicListItem.js
@@ -1,31 +1,52 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import {decorate as mixin} from 'react-mixin'
-import PureRenderMixin from 'react-addons-pure-render-mixin';
+import PureRenderMixin from 'react-addons-pure-render-mixin'
 import {
     View,
     Text,
+    Image,
     StyleSheet
 } from 'react-native'
+import moment from 'moment'
+import Color from '../../constant/Color'
 
-// @mixin(PureRenderMixin)
-class TopicListItem extends Component{
+@mixin(PureRenderMixin)
+class TopicListItem extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
-    render(){
+    render() {
         const {
-            title,content,create_at,last_reply_at,tab,author,reply_count,visit_count
+            title, create_at, last_reply_at, tab, author, reply_count, visit_count
         } = this.props;
 
         return (
             <View style={styles.container}>
-                <View>
-                    <Text>{'饿了么大前端 Node.js 进阶教程'}</Text>
+                <View style={styles.title}>
+                    <View style={styles.tabView}>
+                        <Text style={styles.tabText}>{'精华'}</Text>
+                    </View>
+                    <Text style={styles.titleContent}>{title}</Text>
                 </View>
-                <View>
-
+                <View style={styles.detail}>
+                    <Image
+                        style={styles.avatar}
+                        source={{uri: author.avatar_url}}
+                    />
+                    <View style={styles.centerContent}>
+                        <Text style={styles.detailText}>{author.loginname}</Text>
+                        <Text style={styles.detailText}>{'创建于:' +moment(create_at).format('lll')}</Text>
+                    </View>
+                    <View style={styles.rightContent}>
+                        <Text style={styles.detailText}>
+                            <Text style={{color: '#80bd01'}}>{reply_count}</Text>
+                            {` / ${visit_count}`}
+                        </Text>
+                        <Text style={styles.detailText}>{moment(last_reply_at).startOf('hour').fromNow()
+                        }</Text>
+                    </View>
                 </View>
             </View>
         );
@@ -35,8 +56,60 @@ class TopicListItem extends Component{
 
 const styles = StyleSheet.create({
     container: {
+        height: 100,
+        flexDirection: 'column',
         borderBottomWidth: 0.5,
-        borderBottomColor: '#FFF'
+        borderBottomColor: '#FFF',
+        padding: 15
+    },
+    title: {
+        height: 20,
+        flexDirection: 'row',
+        marginBottom: 10
+    },
+    tabView: {
+        width: 40,
+        height: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
+        backgroundColor: '#80bd01'
+    },
+    tabText: {
+        color: '#FFF'
+    },
+    titleContent: {
+        flex: 1,
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginLeft: 10
+    },
+    detail: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    avatar: {
+        width: 40,
+        height: 40,
+        borderRadius: 20
+    },
+    centerContent: {
+        flex: 1,
+        marginLeft: 10,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+    },
+    rightContent: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        width: 70
+    },
+    detailText: {
+        fontSize: 13,
+        fontFamily: 'Helvetica',
+        color: '#888'
     }
 
 })
---
+++ constant/Color.js
@@ -0,0 +1,11 @@
+const Color  = {
+    top: "#80bd01",
+    good: "#80bd01",
+    ask: "#e5e5e5",
+    job: "#e5e5e5",
+    share: "#e5e5e5",
+}
+
+export default Color
+
+
